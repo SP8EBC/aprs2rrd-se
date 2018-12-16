a07c5b7ef0bbc7d774048c830095c2361b707504
Mateusz Lubecki <sp8ebc@interia.pl>
2018-12-16 14:51:05 +0100

    Release makefiles

---
+++ Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lconfig -lboost_regex -lboost_thread -lpthread -lboost_system -lboost_unit_test_framework -lconfig++ -lmysqlpp
+
---
+++ Release/src/subdir.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+../src/AprsAsioThread.cpp \
+../src/AprsPacket.cpp \
+../src/AprsThread.cpp \
+../src/AprsThreadConfig.cpp \
+../src/AprsWXData.cpp \
+../src/ConnectionTimeoutEx.cpp \
+../src/DataPresentation.cpp \
+../src/MySqlConnInterface.cpp \
+../src/ProgramConfig.cpp \
+../src/ProgramConfig_test.cpp \
+../src/SlewRateLimiter.cpp \
+../src/Telemetry.cpp \
+../src/main.cpp 
+
+OBJS += \
+./src/AprsAsioThread.o \
+./src/AprsPacket.o \
+./src/AprsThread.o \
+./src/AprsThreadConfig.o \
+./src/AprsWXData.o \
+./src/ConnectionTimeoutEx.o \
+./src/DataPresentation.o \
+./src/MySqlConnInterface.o \
+./src/ProgramConfig.o \
+./src/ProgramConfig_test.o \
+./src/SlewRateLimiter.o \
+./src/Telemetry.o \
+./src/main.o 
+
+CPP_DEPS += \
+./src/AprsAsioThread.d \
+./src/AprsPacket.d \
+./src/AprsThread.d \
+./src/AprsThreadConfig.d \
+./src/AprsWXData.d \
+./src/ConnectionTimeoutEx.d \
+./src/DataPresentation.d \
+./src/MySqlConnInterface.d \
+./src/ProgramConfig.d \
+./src/ProgramConfig_test.d \
+./src/SlewRateLimiter.d \
+./src/Telemetry.d \
+./src/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.cpp
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C++ Compiler'
+	g++ -std=c++0x -I/usr/include/mysql++ -I/usr/include/mysql -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
