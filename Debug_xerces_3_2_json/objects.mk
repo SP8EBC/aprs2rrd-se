f4c65330401f9153a5f33cfad3dd1ee41b6db1e3
Mateusz Lubecki <sp8ebc@interia.pl>
2021-12-20 09:48:02 +0100

    new build configuration

---
+++ Debug_xerces_3_2_json/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lmysqlpp -lxerces-c-3.2 -lmysqlclient -lconfig -lconfig++ -lcurl -lpthread -lboost_regex -lboost_thread -lboost_system -lboost_unit_test_framework -lboost_date_time
+
