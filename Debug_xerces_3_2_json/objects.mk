2840d71602beceedf1522b76d14496abf5711329
Mateusz Lubecki <sp8ebc@interia.pl>
2021-12-25 11:59:33 +0100

    local copy of nlohman JSON library in correct version

--- Debug_xerces_3_2_json/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lmysqlpp -lxerces-c-3.2 -lmysqlclient -lconfig -lconfig++ -lcurl -lpthread -lboost_regex -lboost_thread -lboost_system -lboost_unit_test_framework -lboost_date_time
-
