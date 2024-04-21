5ae5f1cb1e52e8739f88678d4b25e82e6370dcda
Mateusz Lubecki <sp8ebc@interia.pl>
2024-04-21 11:13:40 +0200

    lohmann json library version upgraded to 3.8

--- src/Thingspeak.h
+++ src/Thingspeak.h
@@ -12,7 +12,9 @@
 #include <optional>
 #include "TimeTools.h"
 #include "AprsWXData.h"
-#include "../lib/nlohman_json_3_7_1/include/nlohmann/json.hpp"
+//#include "../lib/nlohman_json_3_7_1/include/nlohmann/json.hpp"
+#include "nlohmann/json.hpp"
+
 
 /// @brief 
 struct Thingspeak_Config {
